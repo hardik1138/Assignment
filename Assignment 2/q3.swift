9c6bf3d174a0ce54d7f4e123c5d497d3527c0ae9
Hardik <31307659+hardik1138@users.noreply.github.com>
2021-10-04 14:22:03 +0530

    Create q3.swift

---
+++ Assignment 2/q3.swift
@@ -0,0 +1,14 @@
+print("Enter no of rows:")
+var n = Int (readLine()!)
+n=n+1
+for i in 1..<n{
+    for j in 1..<n{
+        if(j < n-i)
+        {
+            print(" ", terminator: "")
+        }else{
+            print("*",terminator: "")
+        }
+    }
+    print("")
+}
