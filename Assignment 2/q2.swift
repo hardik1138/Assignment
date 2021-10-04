441829f1e17be0bae500c784aba850ab11baaefe
Hardik <31307659+hardik1138@users.noreply.github.com>
2021-10-04 14:18:28 +0530

    Create q2.swift

---
+++ Assignment 2/q2.swift
@@ -0,0 +1,12 @@
+var n = Int (readLine()!)
+var c = 0,i=0
+while i<n{
+    if c > i{
+        print("")
+        c = 0
+        i += 1
+    }else{
+        print("*", terminator: "")
+        c += 1
+    }
+}
